8383622531aed6f9d7d8457962128a6a013feccd
Beaumanoir <theo.beaumanoir@gmail.com>
2026-06-10 13:48:47 +0200

    fix a la con ca va pas marcher propre

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,30 +1,32 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://transcendence42-production.up.railway.app';
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+        destination: `${BACKEND_URL}/api/:path*`,
       },
       {
         source: '/socket.io/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/socket.io/:path*`,
+        destination: `${BACKEND_URL}/socket.io/:path*`,
       },
       {
         source: '/illustrations/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/illustrations/:path*`,
+        destination: `${BACKEND_URL}/illustrations/:path*`,
       },
       {
         source: '/avatars/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/avatars/:path*`,
+        destination: `${BACKEND_URL}/avatars/:path*`,
       },
     ];
   },
-    compiler: {
-        removeConsole: true,
-    }
+  compiler: {
+    removeConsole: true,
+  }
 };
 
 const withNextIntl = createNextIntlPlugin();
